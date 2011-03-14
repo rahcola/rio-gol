9cf646cf8bec8c7bf3f7f210785029e36af6e8cf
Jani Rahkola <jani.rahkola@gmail.com>
2011-03-14 23:50:01 +0200

    blob

--- src/gol/Main.java
+++ src/gol/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		GameOfLife game = new Minimal();
 		long start = System.currentTimeMillis();
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 500; i++) {
 			game.step();
 		}
 		long stop = System.currentTimeMillis();
