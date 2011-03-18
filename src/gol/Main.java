be018abb21b81ac2aac64957c0a87499b55bf8b5
Jani Rahkola <jani.rahkola@gmail.com>
2011-03-18 10:54:38 +0200

    blob

--- src/gol/Main.java
+++ src/gol/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
+
 		GameOfLife game = new Minimal();
 		long start = System.currentTimeMillis();
 		for (int i = 0; i < 100; i++) {
